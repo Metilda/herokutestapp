7a1141989c0da4dbe3a0414430cdcd0b2bda37d2
root <root@ubuntu.(none)>
2012-12-31 15:19:48 +0530

    Commiting the changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Herokutestapp::Application.config.secret_token = '0354469d453d56d4c2e14c9010cd670e86ad9ce2a8ba40fea978457e7a97b4106464b1a8e093bad8f107f51e5978f8a0fe14bef49de334e229283e62f135e611'
